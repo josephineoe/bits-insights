bea918ae4b7d2bd8866d246d378142239d22d791
Mike Zeng <18938909629@163.com>
2025-11-27 22:37:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -297,8 +297,6 @@ def forum_thread(thread_id):
     return render_template("thread.html", thread_id=thread_id)
 
 
-
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
